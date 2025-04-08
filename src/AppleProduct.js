bbda9d8c1b80d5cbf0c2ab02bb5162578e3527e3
Luis Alejandro Carrillo Valdez <151704695+Carrillo-coder@users.noreply.github.com>
2025-04-08 15:45:15 +0000

    update

--- src/AppleProduct.js
+++ src/AppleProduct.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import iphone from "./svg/iphone.svg"
+import airpods from "./svg/airpods.svg"
+import imac from "./svg/imac.svg"
+import macbook from "./svg/macbook.svg"
+import ipad from "./svg/ipad.svg"
+import watch from "./svg/watch.svg"
+
+const mapsSVG = {iphone,ipad, watch, imac, macbook, airpods}
+
 export function AppleProduct( props ) {
   return (
     <div>
       Producto {props.type}
+      <img alt="producto" src={mapsSVG[props.type]} width={200} /> 
     </div>
   )
 }
